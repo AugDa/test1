ec5f56053308654e2b8d2a1a3af97ea2b3e92344
AugDa <90198657+AugDa@users.noreply.github.com>
2021-09-20 17:10:48 +0200

    Add files via upload

---
+++ zad1.php
@@ -0,0 +1,53 @@
+<?php
+
+//Ćw 1
+/*$t = 6;
+echo gettype($t).'<br>';
+$a = [1 => 'wartosc1', 2 => 'element_drugi', 3 =>'wartosc3'];
+echo var_dump($a).'<br>';
+
+if( is_array($a) ){
+echo 'true';}
+else{
+    echo 'false';
+}*/
+
+//Ćw 2
+/*define('imie','Augustyn');
+define('kierunek',"Techink programista");
+echo 'Jestem '.imie.' i jestem na kierunku '.kierunek;
+*/
+
+//zad1
+/*define('pln',4.60);
+define('euro',4);
+echo euro*pln;*/
+
+//zad2
+/*$x=2;
+$y=3;
+$z=5;
+$x++;   //postinkrementacja zwiększa wartość zmiennej o 1 po wykonaniu polecenia x=3
+$y--;   //postdekrementacja zmniejsza wartość zmiennej o 1 po wykonaniu polecenia y=2
+$z += $x;   //przypisuję zmniennej z wartość dodawania z+x z = 8
+--$z;   //predekrementacja zmiejsza wartość o 1 przed wykonaniem polecenia
+*/
+
+//zad3
+/*define('liczba1',5642);
+define('liczba2',80);
+echo "Liczba 1=".liczba1.'<br>';
+echo "Liczba 2=".liczba2.'<br>';
+echo "Suma liczb:".'<br>'.liczba1+liczba2.'<br>';
+echo "Różnica liczb:".'<br>'.liczba1-liczba2.'<br>';
+echo "Iloczyn liczb:".'<br>'.liczba1*liczba2.'<br>';
+echo "Suma liczb:".'<br>'.liczba1/liczba2.'<br>';*/
+
+//zad4
+/*$x=7;
+$y=3;
+echo ($x + 2 * $y)/2;*/
+
+
+
+?>
