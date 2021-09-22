2b4d9f5253e884b30ccfd562d433b0d5b172841f
Augustyn Dampc <90198657+AugDa@users.noreply.github.com>
2021-09-22 10:28:13 +0200

    Add files via upload

---
+++ wiek_sredni.py
@@ -0,0 +1,7 @@
+S=int(input('podaj średnią wieku osób'))
+X=int(input('podaj wiek najmłodzszej osoby'))
+Y=int(input('podaj wiek najstarszej osoby'))
+Z=(S*3)-X-Y
+print('wiek trzeciej osoby wynosi',Z)
+
+
