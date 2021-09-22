2ae08b7f00dbcaf5505fc3715cea77c60be7f50c
Augustyn Dampc <90198657+AugDa@users.noreply.github.com>
2021-09-22 10:29:52 +0200

    Add files via upload

---
+++ salatka.py
@@ -0,0 +1,17 @@
+o1=1
+print('liczba osób uwzględnianych w przepisie osób dla',o1)
+sk1=0.5
+print(sk1,'ilość kg jabłka dla ', o1,' osób/osoby')
+sk2=5
+print(sk2,'ilość kg malin dla ', o1,' osób/osoby')
+sk3=3
+print(sk3,'ilość kg brzoskwiń dla ', o1,' osób/osoby')
+o2=2
+print('liczba uczestników spotkania',o2)
+
+s2=o2*sk1
+print(s2,'ilość składnika dla ', o2,' osób')
+s2=o2*sk2
+print(s2,'ilość składnika dla ', o2,' osób')
+s2=o2*sk3
+print(s2,'ilość składnika dla ', o2,' osób')
